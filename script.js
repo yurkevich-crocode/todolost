53f5cb503f7eb477e30b9b0ef631eb683c74c988
Daniil Yurkevich <Ddaniilsergeevich200111@gmail.com>
2023-02-24 18:02:13 +0300

    intermediate

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ window.onload = () => {
   const addTodo = document.getElementById("addTodo");
   const text = document.getElementById("text");
   const colorPicker = document.getElementById("color");
-
+  const crossBtn = document.querySelector(".todo__add-close");
   const todoWrapper = document.querySelector(".todo__wrapper");
+  const allBtn = document.querySelector(".todo__filter-all");
+  const completedBtn = document.querySelector(".todo__filter-completed");
+  const expectBtn = document.querySelector(".todo__filter-expect");
 
   function render() {
     list.innerHTML = " ";
@@ -20,25 +23,78 @@ window.onload = () => {
       localStorage.setItem(__STORAGE_NAME, JSON.stringify([]));
     }
 
-    console.log(!JSON.parse(localStorage.getItem(__STORAGE_NAME)).length);
-
     let storageArr = localStorage.getItem(__STORAGE_NAME);
     let newArr = JSON.parse(storageArr);
     if (localStorage.length != 0) {
       newArr.forEach((element) => {
         if (!list.classList.contains("hide")) {
           list.innerHTML += `
-        <div class="todo__item" data-id="${element.id}" style="background-color:${element.color}"> 
-            <span class="todo__item-text">${element.text}</span>
-            <div class="todo__controls">
-                <button class="todo__control todo__control-accept"></button>
-                <button class="todo__control todo__control-delete"></button>
+            <div class="todo__item" data-id="${element.id}" style="--label-color:${element.color}" data-status="${element.status}"> 
+                <span class="todo__item-text">${element.text}</span>
+                <div class="todo__controls">
+                    <button class="todo__control todo__control-accept"></button>
+                    <button class="todo__control todo__control-delete"></button>
+                </div>
             </div>
-        </div>
-        `;
+            `;
         }
       });
     }
+
+    allBtn.addEventListener("click", () => {
+      list.innerHTML = "";
+      newArr.forEach((element) => {
+        if (!list.classList.contains("hide")) {
+          list.innerHTML += `
+            <div class="todo__item" data-id="${element.id}" style="--label-color:${element.color}" data-status="${element.status}"> 
+                <span class="todo__item-text">${element.text}</span>
+                <div class="todo__controls">
+                    <button class="todo__control todo__control-accept"></button>
+                    <button class="todo__control todo__control-delete"></button>
+                </div>
+            </div>
+            `;
+        }
+      });
+    });
+
+    completedBtn.addEventListener("click", () => {
+      list.innerHTML = "";
+      newArr.forEach((element) => {
+        if (!list.classList.contains("hide")) {
+          if (element.status == "completed") {
+            list.innerHTML += `
+              <div class="todo__item" data-id="${element.id}" style="--label-color:${element.color}" data-status="${element.status}"> 
+                  <span class="todo__item-text">${element.text}</span>
+                  <div class="todo__controls">
+                      <button class="todo__control todo__control-accept"></button>
+                      <button class="todo__control todo__control-delete"></button>
+                  </div>
+              </div>
+              `;
+          }
+        }
+      });
+    });
+
+    expectBtn.addEventListener("click", () => {
+      list.innerHTML = "";
+      newArr.forEach((element) => {
+        if (!list.classList.contains("hide")) {
+          if (element.status == "expect") {
+            list.innerHTML += `
+              <div class="todo__item" data-id="${element.id}" style="--label-color:${element.color}" data-status="${element.status}"> 
+                  <span class="todo__item-text">${element.text}</span>
+                  <div class="todo__controls">
+                      <button class="todo__control todo__control-accept"></button>
+                      <button class="todo__control todo__control-delete"></button>
+                  </div>
+              </div>
+              `;
+          }
+        }
+      });
+    });
 
     if (localStorage.length != 0) {
       const todoItemAll = document.querySelectorAll(".todo__item");
@@ -46,18 +102,27 @@ window.onload = () => {
         const deleteCurDataAll = e.querySelectorAll(".todo__control-delete");
         deleteCurDataAll.forEach((element) => {
           element.addEventListener("click", () => {
+            console.log(element);
             const pos = newArr.map((e) => e.id);
             const indexId = pos.indexOf(
               +element.parentNode.parentNode.dataset.id
             );
             if (indexId == 0) {
-              newArr.splice(0, 1);
-              localStorage.setItem(__STORAGE_NAME, JSON.stringify(newArr));
-              render();
+              e.classList.add("delete");
+              setTimeout(() => {
+                e.classList.remove("delete");
+                newArr.splice(0, 1);
+                localStorage.setItem(__STORAGE_NAME, JSON.stringify(newArr));
+                render();
+              }, 200);
             } else {
-              newArr.splice(indexId, 1);
-              localStorage.setItem(__STORAGE_NAME, JSON.stringify(newArr));
-              render();
+              e.classList.add("delete");
+              setTimeout(() => {
+                newArr.splice(indexId, 1);
+                localStorage.setItem(__STORAGE_NAME, JSON.stringify(newArr));
+                e.classList.remove("delete");
+                render();
+              }, 200);
             }
           });
         });
@@ -67,7 +132,6 @@ window.onload = () => {
 
   function openAddField() {
     addWrapper.classList.toggle("active");
-    list.classList.toggle("hide");
     if (addBtn.textContent == "Add") {
       addBtn.textContent = "Back";
       render();
@@ -79,6 +143,9 @@ window.onload = () => {
 
   function addData() {
     const notification = document.querySelector(".todo__notification");
+    const textNotification = notification.querySelector(
+      ".todo__notification-text"
+    );
     const id = Date.now();
     let storage = localStorage.getItem(__STORAGE_NAME);
     if (storage) {
@@ -86,19 +153,31 @@ window.onload = () => {
     } else {
       storage = [];
     }
+
     let obj = {
       id,
       color: color.value,
       text: text.value,
+      status: "expect",
     };
-    console.log(color.value);
-    localStorage.setItem(__STORAGE_NAME, JSON.stringify([...storage, obj]));
-    render();
-    notification.classList.add("active");
 
-    setTimeout(() => {
-      notification.classList.remove("active");
-    }, 3000);
+    if (text.value) {
+      localStorage.setItem(__STORAGE_NAME, JSON.stringify([...storage, obj]));
+      render();
+      notification.classList.add("active");
+      notification.style.backgroundColor = "rgb(87, 183, 87)";
+      textNotification.textContent = "Todo was successfully added";
+      setTimeout(() => {
+        notification.classList.remove("active");
+      }, 3000);
+    } else {
+      notification.classList.add("active");
+      notification.style.backgroundColor = "rgb(238, 31, 31)";
+      textNotification.textContent = "Todo was not created, please enter text";
+      setTimeout(() => {
+        notification.classList.remove("active");
+      }, 3000);
+    }
   }
 
   addBtn.addEventListener("click", () => {
@@ -107,7 +186,14 @@ window.onload = () => {
 
   addTodo.addEventListener("click", () => {
     addData();
+    addTodo.disabled = "true";
+    setTimeout(() => {
+      addTodo.disabled = false;
+    }, 3000);
   });
 
+  crossBtn.addEventListener("click", () => {
+    openAddField();
+  });
   render();
 };
